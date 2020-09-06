739afe45aa079041e4f6830425302a6e0b2fdbe9
Hearot <gabriel@hearot.it>
2020-09-06 23:40:11 +0200

    style: fix the notice in primecount.c

--- primecount.c
+++ primecount.c
@@ -8,17 +8,17 @@
  * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice,
- this
+ * this
  *    list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
-
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- ARE
+ * ARE
  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
@@ -79,12 +79,12 @@ PyObject* phi(PyObject* self, PyObject* args) {
   return PyLong_FromLongLong(primecount_phi(x, a));
 }
 
-#define PRIMECOUNT_DOCSTRING_PI       \
-  "pi(x: int) -> int\n\n"             \
-  "Count the number of primes <= x.\n\n"  \
-  "Args:\n"                           \
-  "    x (int): The upper limit x.\n\n" \
-  "Returns:\n"                        \
+#define PRIMECOUNT_DOCSTRING_PI          \
+  "pi(x: int) -> int\n\n"                \
+  "Count the number of primes <= x.\n\n" \
+  "Args:\n"                              \
+  "    x (int): The upper limit x.\n\n"  \
+  "Returns:\n"                           \
   "    int: How many numbers less than or equal to x are primes."
 
 PyObject* pi(PyObject* self, PyObject* args) {
